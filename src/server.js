d65959f22c17ba4c38b5f5b6af6c7ecd9bbe6673
imran <imrankha12@gmail.com>
2020-12-15 15:20:34 +0530

    dvbhjbvsv

--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/spacecraftapp')); 
-// app.get('*', function(req,res) {
-//   res.sendFile(path.join(__dirname + '/dist/spacecraftapp/index.html'));
-// });
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname + '/dist/spacecraftapp/index.html'));
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 
 
-app.get('*', (req, res) => {
-  res.sendFile(`./dist/spacecraftapp/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(`./dist/spacecraftapp/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
 
