eecdddff16c6369b76b72e6d5b52a64fef3ccacc
imran <imrankha12@gmail.com>
2020-12-15 16:58:33 +0530

    dsgagagG

--- src/server.js
+++ src/server.js
@@ -4,7 +4,10 @@ const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/spacecraftapp')); 
-app.get('*', function(req,res) {
+app.get('', function(req,res) {
+  res.sendFile(path.join(__dirname + '/dist/spacecraftapp/index.html'));
+});
+app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/spacecraftapp/index.html'));
 });
 // app.get('*', (req, res) => {
